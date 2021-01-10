0adf627f58641dc05aa8a5ad94fd94361127cbdb
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-01-10 13:36:06 -0300

    cap01-Aplicando a Constraint Bias

---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.alura.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
